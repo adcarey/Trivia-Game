893cc04d8ad7dabb7bdbf32d205827d33ba19e53
adcarey <Adam@Adams-MacBook.local>
2017-06-03 11:11:36 -0600

    adding buttons

--- assets/trivia.js
+++ assets/trivia.js
@@ -25,7 +25,7 @@ var right = 0;
 //Functions 
 //Questions
 function startGame(){
-questionsAndAnswers = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questions[questionCount] + "</p><button class='first-answer answer'>A. " + answers[questionCount][0] + "</button><button class='answer'>B. "+answers[questionCount][1]+"</button><button class='answer'>C. "+answers[questionCount][2]+"</button><button class='answer'>D. "+answers[questionCount][3]+"</button>";
+questionsAndAnswers = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questions[questionCount] + "</p><button class='first-answer answer'>A. " + answers[questionCount][0] + "</button><br><button class='answer'>B. "+answers[questionCount][1]+"</button><br><button class='answer'>C. "+answers[questionCount][2]+"</button><br><button class='answer'>D. "+answers[questionCount][3]+"</button>";
 	$("#question").html(questionsAndAnswers);
 }
 function rightResult() {
